1f95c62e484474ba68e9daa254e94cea1e0d4ca5
francJi <minkyuji95@hanmail.net>
2023-09-11 13:38:06 +0900

    [ADD] ProjectileController 스크립트 추가 (민규)

---
+++ Assets/1_Scripts/ProjectileController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour
+{
+    //Stage stage                  // Stage 클래스 추가시 주석 해제
+
+    public float Speed;
+    public Vector3 StartPosition;
+    public Vector3 _Rotation;
+    public Vector3 _Scale;
+    // public Color color;         // Color 가 필요한가.. 의문
+    public Sprite sprite;
+       
+}
