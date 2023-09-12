2e37b7fbde9c07099c62c418ad6be10219283dab
francJi <minkyuji95@hanmail.net>
2023-09-13 01:26:01 +0900

    [UPDATE] 상황별(스테이지별) enemy 다양화를 위한 EnemyController 스크립트 수정 (민규)

--- Assets/1_Scripts/EnemyController.cs
+++ Assets/1_Scripts/EnemyController.cs
@@ -2,23 +2,75 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.SceneManagement;
 using UnityEngine;
+using System;
+
 
 public class EnemyController : MonoBehaviour
 {
     public Enemy EnemySet;
-
-    private void Awake()
+    [Serializable]
+    public class Stage1SpriteInfo
     {
+        public string animatorName;
+        public GameObject enemyPrefab;
     }
+    [SerializeField] Stage1SpriteInfo[] stage1SpriteInfos;
+
+    [Serializable]
+    public class Stage2SpriteInfo
+    {
+        public string animatorName;
+        public GameObject enemyPrefab;
+    }
+    [SerializeField] Stage2SpriteInfo[] stage2SpriteInfos;
+
+    [Serializable]
+    public class Stage3SpriteInfo
+    {
+        public string animatorName;
+        public GameObject enemyPrefab;
+    }
+    [SerializeField] Stage3SpriteInfo[] Stage3SpriteInfos;
+
+    public GameObject[] randEnemyArray = null;
+    public int[] damageArray = null;
 
     public void SetDamage()
     {
         // 유니티에서 직접 집어넣는게 나은가..
     }
 
+    void SetDict()
+    {
+        if (stage1SpriteInfos != null) 
+        {
+            for (int i = 0; i < stage1SpriteInfos.Length; i++) // stage 숫자가 1보다 작거나 같다면의 조건 추가
+            {
+                Array.Resize(ref randEnemyArray, randEnemyArray.Length + 1);
+                randEnemyArray[randEnemyArray.Length - 1] = stage1SpriteInfos[i].enemyPrefab;
+                Array.Resize(ref damageArray, damageArray.Length + 1);
+                damageArray[damageArray.Length - 1] = 1;
+            }
+            for (int i = 0; i < stage2SpriteInfos.Length; i++) // stage 숫자가 2보다 작거나 같다면의 조건 추가
+            {
+                Array.Resize(ref randEnemyArray, randEnemyArray.Length + 1);
+                randEnemyArray[randEnemyArray.Length - 1] = stage2SpriteInfos[i].enemyPrefab;
+                Array.Resize(ref damageArray, damageArray.Length + 1);
+                damageArray[damageArray.Length - 1] = 1;       // stage별 데이지 변화 필요시 변경
+            }
+            for (int i = 0; i < Stage3SpriteInfos.Length; i++) // stage 숫자가 3보다 작거나 같다면의 조건 추가
+            {
+                Array.Resize(ref randEnemyArray, randEnemyArray.Length + 1);
+                randEnemyArray[randEnemyArray.Length - 1] = Stage3SpriteInfos[i].enemyPrefab;
+                Array.Resize(ref damageArray, damageArray.Length + 1);
+                damageArray[damageArray.Length - 1] = 1;       // stage별 데이지 변화 필요시 변경
+            }
+        }
+    }
     private void SummonEnemy()
     {
         EnemySet.SetEnemy();
+       
         if (EnemySet != null)
         {
             var enemyGo = ObjectPoolManager.instance.Pool.Get();
@@ -26,11 +78,18 @@ public class EnemyController : MonoBehaviour
             Debug.Log($"Speed : {EnemySet.Speed}");
             enemyGo.SetActive(true);
             enemyGo.transform.position = EnemySet.StartPosition;
+
+            // animator 적용
+            int randNum = UnityEngine.Random.Range(0, randEnemyArray.Length);
+            Debug.Log($"randNum : {randNum} || stage1Length : {randEnemyArray.Length}");
+            enemyGo.GetComponent<Animator>().runtimeAnimatorController = randEnemyArray[randNum].GetComponent<Animator>().runtimeAnimatorController;
+            enemyGo.GetComponent<Enemy>().Damage = damageArray[randNum];
         }
     }
 
     private void Start()
     {
+        SetDict();
         InvokeRepeating("SummonEnemy", 1f, 1f / EnemySet.Speed);
     }
 }
