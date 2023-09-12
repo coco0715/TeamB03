8db7bbdd2f91198ef2ff0d739e91ce5df29fcef6
coco0715 <ment0715@naver.com>
2023-09-12 19:50:10 +0900

    [ADD & Fix] TopDownController.cs 내부 클래스명 함수 수정 & PlayerController 스크립트 추가 (-하나)

--- Assets/1_Scripts/Controllers/TopDownController.cs
+++ Assets/1_Scripts/Controllers/TopDownController.cs
@@ -3,17 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class TopDownController : MonoBehaviour
 {
     public event Action<Vector2> OnMoveEvent;
-    public event Action<Vector2> OnLookEvent;
     public void CallOnMoveEvent(Vector2 direction)
     {
         OnMoveEvent?.Invoke(direction);
     }
 
-    public void CallOnLookEvent(Vector2 direction)
-    {
-        OnLookEvent?.Invoke(direction);
-    }
 }
