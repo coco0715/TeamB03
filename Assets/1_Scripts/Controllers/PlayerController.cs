d348391066fce80f88f212c9004bbd635fb4d12a
coco0715 <ment0715@naver.com>
2023-09-12 19:57:42 +0900

    [ADD] PlayerMovement 스크립트 추가 (-하나)

--- Assets/1_Scripts/Controllers/PlayerController.cs
+++ Assets/1_Scripts/Controllers/PlayerController.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : TopDownController
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnMove(InputValue value)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Vector2 moveInput = value.Get<Vector2>().normalized;
+        CallOnMoveEvent(moveInput);
     }
 }
