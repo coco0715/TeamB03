af14bd7727c4ca09c8b7f006195536c3f7614511
francJi <minkyuji95@hanmail.net>
2023-09-12 20:14:01 +0900

    [UPDATE] CoinController 수정 (민규)

--- Assets/1_Scripts/CoinController.cs
+++ Assets/1_Scripts/CoinController.cs
@@ -2,46 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CoinController : Projectile
+public class CoinController : MonoBehaviour
 {
-    public float CoinSpeed;
-    public GameObject Coin;
-    //public int Score;
-
-    private void Awake()
+    public Coin CoinObj;
+    private void DropCoins()
     {
-        // GenCoeff = Stage.GenCountCoeff;
-        // SetCount();
-        GenCount = 3;
-
-        // SpeedCoeff = Stage.CoinSpeedCoeff;
-        CoinSpeed = 1.5f;     // 왜 위에서는 안되고 여기서는 되는가.
-        Speed = CoinSpeed;
-        SpeedCoeff = 1.0f;
-        SetSpeed();
-
-        // ScaleCoeff = Stage.EnemyScaleCoeff;
-        ScaleCoeff = 1.0f;
-        SetScale();
-
-        StartPosition = SetStartPosition();
-    }
-
-    private void Update()
-    {
-        Coin.transform.position += new Vector3(0f, -1f, 0f) * Speed * Time.deltaTime;
-    }
-
-    public void OnTriggerEnter2D(Collider2D coll)
-    {
-        // 점수 연결
-        // Score = GameManager.Instance.Score;
-        
-        if (coll.gameObject.tag == "Player")
+        CoinObj.SetCoin();
+        if (CoinObj != null)
         {
-            // player.Score += 10;  // 코인 콤보시스템에 의한 점수 추가해야할듯?
-            Debug.Log("10점 추가");
-            Destroy(Coin);
+            Instantiate(CoinObj);
         }
+    }
+
+    private void Start()
+    {
+        InvokeRepeating("DropCoins", 1f, 0.5f);
     }
 }
