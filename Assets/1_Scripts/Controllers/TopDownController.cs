bba11d505907d225a7d1810c825c6226a1567392
eohalim <dhl612@naver.com>
2023-09-13 15:51:11 +0900

    [ADD]InputSystem (-하림)
    Look, FIre 추가
    PlayerController.cs에 움직임, 시선 이벤트 처리 코드 추가 (팀원 검수 요망)

--- Assets/1_Scripts/Controllers/TopDownController.cs
+++ Assets/1_Scripts/Controllers/TopDownController.cs
@@ -6,9 +6,16 @@ using UnityEngine;
 public class TopDownController : MonoBehaviour
 {
     public event Action<Vector2> OnMoveEvent;
+    public event Action<Vector2> OnLookEvent;
+
     public void CallOnMoveEvent(Vector2 direction)
     {
         OnMoveEvent?.Invoke(direction);
+    }
+
+    public void CallOnLookEvent(Vector2 direction)
+    {
+        OnLookEvent?.Invoke(direction);
     }
 
 }
