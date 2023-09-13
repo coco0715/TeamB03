8e3d665e08d900f35b087474efdd9c08335e89f9
eohalim <dhl612@naver.com>
2023-09-13 15:01:43 +0900

    [ADD]Player InputSystem (-하림)
    마우스 추가(OnLook, OnFire)

--- Assets/1_Scripts/Controllers/PlayerController.cs
+++ Assets/1_Scripts/Controllers/PlayerController.cs
@@ -5,9 +5,32 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : TopDownController
 {
+    private Camera _camera;
+    private void Awake()
+    {
+        _camera = Camera.main;
+    }
+
     public void OnMove(InputValue value)
     {
         Vector2 moveInput = value.Get<Vector2>().normalized;
         CallOnMoveEvent(moveInput);
+    }
+    
+    public void OnLook(InputValue value)
+    {
+        Vector2 newAim = value.Get<Vector2>();
+        Vector2 worldPos = _camera.ScreenToWorldPoint(newAim);
+        newAim = (worldPos - (Vector2)transform.position).normalized;
+
+        if(newAim.magnitude >= .9f)
+        {
+            CallOnLookEvent(newAim); 
+        }
+    }
+
+    public void OnFire(InputValue value)
+    {
+        Debug.Log("OnFire" + value.ToString());
     }
 }
